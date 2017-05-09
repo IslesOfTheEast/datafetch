c0f94a641e66db299e1a0859afd953414fa9106d
IslesOfTheEast <islesoftheeast@protonmail.ch>
2017-05-09 18:06:51 +0530

    initial project version

---
+++ README.md
@@ -0,0 +1,9 @@
+# datafetch
+### [WIP]
+
+A screenfetch clone written in python that displays a lot more info, a WIP currently so there's an awful lot of stuff that are yet to be added. 
+
+### todo list:
+
+* ascii art for linux distributions
+* informating about the desktop's resolution, uptime, gpu and terminal emulator 
---
+++ source.py
@@ -0,0 +1,26 @@
+import os
+import platform
+import socket
+import psutil
+
+	
+user = os.getlogin()
+cwd = os.getcwd()
+hostname = socket.gethostname()
+processor = platform.processor()
+system = platform.platform()
+cpu_usage = psutil.cpu_percent(interval=1)
+cpu_count = psutil.cpu_count()
+mem = psutil.virtual_memory()
+
+def get_info():
+	
+	if platform.system() == "Windows": 
+		ascii_art = open("ascii\windows.txt", 'r')
+		content = ascii_art.read()
+		print(content.format(hostname, user, system, processor, cpu_usage, cpu_count, 
+			str(mem.total//(1024**2)) + "/" + str(mem.used//(1024**2)) + " MB", mem.percent))
+
+	
+
+get_info()
